4e8e2d5589474f89e2495b61c889ede1bcd29ba5
Connor <cbroyles94@gmail.com>
2018-07-08 14:48:31 -0400

    Formatting updates

--- Engine/src/scene/scene.h
+++ Engine/src/scene/scene.h
@@ -9,15 +9,51 @@ class Engine;
 
 class Scene {
 public:
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	Scene(const Scene& scene) = delete;
+
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	virtual ~Scene() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
+	Scene& operator=(const Scene& scene) = delete;
+
+
+	//----------------------------------------------------------------------------------
+	// Member Functions - Misc
+	//----------------------------------------------------------------------------------
+
+	// Load the scene contents
 	virtual void load(const Engine& engine) = 0;
 
+	// Unload the scene contents
 	void unload(const Engine& engine);
 
 	// Update the scene
 	virtual void tick(const Engine& engine) = 0;
+
+	[[nodiscard]]
+	const string& getName() const {
+		return name;
+	}
+
+	[[nodiscard]]
+	const map<string, Text>& getTexts() const {
+		return texts;
+	}
+
+
+	//----------------------------------------------------------------------------------
+	// Member Functions - Entities
+	//----------------------------------------------------------------------------------
 
 	// Add an entity to this scene
 	template<typename EntityT, typename... ArgsT>
@@ -33,25 +69,11 @@ public:
 		ecs_engine.destroyEntity(entity);
 	}
 
-
-	//----------------------------------------------------------------------------------
-	// Getters
-	//----------------------------------------------------------------------------------
-
-	[[nodiscard]]
-	const string& getName() const {
-		return name;
-	}
-
-	[[nodiscard]]
-	const map<string, Text>& getTexts() const {
-		return texts;
-	}
-
 	[[nodiscard]]
 	const vector<handle64>& getEntities() const {
 		return entities;
 	}
+
 
 
 protected:
@@ -59,14 +81,20 @@ protected:
 		: name("Scene") {
 	}
 
-	Scene(string&& name)
+	Scene(string name)
 		: name(std::move(name)) {
 	}
 
 	Scene(Scene&& scene) = default;
 
+	Scene& operator=(Scene&& scene) = default;
+
 
 protected:
+	//----------------------------------------------------------------------------------
+	// Member Variables
+	//----------------------------------------------------------------------------------
+
 	string name;
 	map<string, Text> texts;
 	vector<handle64> entities;
