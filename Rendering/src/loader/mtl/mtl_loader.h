559220e37fcc2b158a3b7c8567315fbe2daef045
Connor <cbroyles94@gmail.com>
2018-10-19 21:18:32 -0400

    Add comments.

--- Rendering/src/loader/mtl/mtl_loader.h
+++ Rendering/src/loader/mtl/mtl_loader.h
@@ -8,24 +8,40 @@ class ResourceMgr;
 
 class MtlLoader final : public LineReader {
 public:
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	MtlLoader(ResourceMgr& resource_mgr, std::vector<Material>& materials);
 	MtlLoader(const MtlLoader& loader) = delete;
 	MtlLoader(MtlLoader&& loader) = delete;
 
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~MtlLoader() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	MtlLoader& operator=(const MtlLoader& loader) = delete;
 	MtlLoader& operator=(MtlLoader&& loader) = delete;
 
 
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	void load(const fs::path& file);
-
 
 private:
 	void readLine() override;
 
 
 private:
+	//----------------------------------------------------------------------------------
+	// Member Variables
+	//----------------------------------------------------------------------------------
 	ResourceMgr& resource_mgr;
 	std::vector<Material>& materials;
 };
