74be3cfc93a54ecd5ea9f9b8da269f6588cc94de
Connor <cbroyles94@gmail.com>
2018-10-25 21:38:09 -0400

    Made the ThrowIfFailed related macros more general and expressive.

--- Utilities/src/exception/exception.h
+++ Utilities/src/exception/exception.h
@@ -12,16 +12,30 @@
 #include <iostream>
 
 
+//----------------------------------------------------------------------------------
+// Defines
+//----------------------------------------------------------------------------------
+
+// Utility macros
+#define EXPAND(...) __VA_ARGS__
+#define CAT(a, ...) a ## __VA_ARGS__
+
+// Macro selection based on argument count
+#define SELECT(name, num) CAT(name##_, num)
+#define GET_COUNT(_1, _2, _3, _4, _5, _6, _7, _8, count, ...) count
+#define VA_SIZE(...) EXPAND(GET_COUNT(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1))
+#define VA_SELECT(name, ...) SELECT(name, VA_SIZE(__VA_ARGS__))(__VA_ARGS__)
+
+// ThrowIfFailed macros
+#define ThrowIfFailed_1(result) ThrowIfFailed(result, __FILE__, __LINE__)
+#define ThrowIfFailed_2(result, msg) ThrowIfFailed(result, __FILE__, __LINE__, msg)
+
+#define ThrowIfFailed(...) VA_SELECT(ThrowIfFailed, __VA_ARGS__)
+
 
 //----------------------------------------------------------------------------------
 // Functions
 //----------------------------------------------------------------------------------
-
-#define EXPAND(x) x
-#define GET_THROW_MACRO(_2, _1, NAME, ...) NAME
-#define ThrowIfFailed_1(result) ThrowIfFailed(result, __FILE__, __LINE__)
-#define ThrowIfFailed_2(result, msg) ThrowIfFailed(result, __FILE__, __LINE__, msg)
-#define ThrowIfFailed(...) EXPAND(GET_THROW_MACRO(__VA_ARGS__, ThrowIfFailed_2, ThrowIfFailed_1)(__VA_ARGS__))
 
 inline void (ThrowIfFailed)(bool result, const char* file, int line, const std::string& msg = "Unknown Error") {
 	if (!result) {
