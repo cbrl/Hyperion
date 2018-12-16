a95597dc51df6893ba40348939b259e33edb6977
Connor <cbroyles94@gmail.com>
2018-12-16 15:35:48 -0500

    Removed world_object.h. Moved core entity templates to the core_entities header.

--- Rendering/src/scene/entities/core_entities.h
+++ Rendering/src/scene/entities/core_entities.h
@@ -1,5 +1,23 @@
 #pragma once
 
-namespace EntityTemplates {};
+#include "entity/entity.h"
+#include "scene/components/transform/transform.h"
 
-#include "world_object.h"
+
+namespace EntityTemplates {
+
+// Does nothing to the specified entity
+struct Empty final {
+	static void applyTemplate(Entity& entity) {};
+};
+
+
+// Adds a transform component to the specified entity. The basic
+// template for any non-specific entity with a position in the world.
+struct WorldObject final {
+	static void applyTemplate(Entity& entity) {
+		entity.addComponent<Transform>();
+	}
+};
+
+} // namespace EntityTemplates
