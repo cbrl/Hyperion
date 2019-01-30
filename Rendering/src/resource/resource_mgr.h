e804935291a0d43ba134e0fe8ba7b5b6c8885a06
Connor <cbroyles94@gmail.com>
2019-01-30 18:59:11 -0500

    Added functions to get a const reference to the resource maps in ResourceMgr

--- Rendering/src/resource/resource_mgr.h
+++ Rendering/src/resource/resource_mgr.h
@@ -25,7 +25,6 @@ public:
 	//----------------------------------------------------------------------------------
 	// Constructors
 	//----------------------------------------------------------------------------------
-
 	ResourceMgr(ID3D11Device& device, ID3D11DeviceContext& device_context)
 		: device(device)
 		, device_context(device_context) {
@@ -38,14 +37,12 @@ public:
 	//----------------------------------------------------------------------------------
 	// Destructor
 	//----------------------------------------------------------------------------------
-
 	~ResourceMgr() = default;
 
 
 	//----------------------------------------------------------------------------------
 	// Operators
 	//----------------------------------------------------------------------------------
-
 	ResourceMgr& operator=(const ResourceMgr& mgr) = delete;
 	ResourceMgr& operator=(ResourceMgr&& mgr) noexcept = default;
 
@@ -64,9 +61,16 @@ public:
 	template<typename ResourceT, typename VertexT>
 	[[nodiscard]]
 	auto acquire(const std::wstring& name,
-	                 const ModelOutput& model_data,
-	                 const ModelConfig<VertexT>& config)
+	             const ModelOutput& model_data,
+	             const ModelConfig<VertexT>& config)
 	    -> std::enable_if_t<std::is_same_v<ModelBlueprint, ResourceT>, std::shared_ptr<ModelBlueprint>>;
+
+
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+		-> std::enable_if_t<std::is_same_v<ModelBlueprint, ResourceT>, const resource_map<std::wstring, ModelBlueprint>&>;
+		
 
 
 	//----------------------------------------------------------------------------------
@@ -83,9 +87,14 @@ public:
 	template<typename ResourceT>
 	[[nodiscard]]
 	auto acquire(const std::wstring& name,
-	                 const D3D11_TEXTURE2D_DESC& desc,
-	                 const D3D11_SUBRESOURCE_DATA& init_data)
+	             const D3D11_TEXTURE2D_DESC& desc,
+	             const D3D11_SUBRESOURCE_DATA& init_data)
 	    -> std::enable_if_t<std::is_same_v<Texture, ResourceT>, std::shared_ptr<Texture>>;
+
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+		-> std::enable_if_t<std::is_same_v<Texture, ResourceT>, const resource_map<std::wstring, Texture>&>;
 
 
 	//----------------------------------------------------------------------------------
@@ -98,9 +107,14 @@ public:
 	auto acquire(const std::wstring& filename)
 	    -> std::enable_if_t<std::is_same_v<Font, ResourceT>, std::shared_ptr<Font>>;
 
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+		-> std::enable_if_t<std::is_same_v<Font, ResourceT>, const resource_map<std::wstring, Font>&>;
+
 
 	//----------------------------------------------------------------------------------
-	// Member Functions - Shader
+	// Member Functions - Compute Shader
 	//----------------------------------------------------------------------------------
 
 	// Create a resource, or retrieve it if it already exists.
@@ -109,11 +123,31 @@ public:
 	auto acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
 	    -> std::enable_if_t<std::is_same_v<ComputeShader, ResourceT>, std::shared_ptr<ComputeShader>>;
 
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+		-> std::enable_if_t<std::is_same_v<ComputeShader, ResourceT>, const shader_resource_map<std::wstring, ComputeShader>&>;
+
+
+	//----------------------------------------------------------------------------------
+	// Member Functions - Domain Shader
+	//----------------------------------------------------------------------------------
+
 	// Create a resource, or retrieve it if it already exists.
 	template<typename ResourceT>
 	[[nodiscard]]
 	auto acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
 	    -> std::enable_if_t<std::is_same_v<DomainShader, ResourceT>, std::shared_ptr<DomainShader>>;
+
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+	    -> std::enable_if_t<std::is_same_v<DomainShader, ResourceT>, const shader_resource_map<std::wstring, DomainShader>&>;
+
+
+	//----------------------------------------------------------------------------------
+	// Member Functions - Geometry Shader
+	//----------------------------------------------------------------------------------
 
 	// Create a resource, or retrieve it if it already exists.
 	template<typename ResourceT>
@@ -121,11 +155,31 @@ public:
 	auto acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
 	    -> std::enable_if_t<std::is_same_v<GeometryShader, ResourceT>, std::shared_ptr<GeometryShader>>;
 
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+	    -> std::enable_if_t<std::is_same_v<GeometryShader, ResourceT>, const shader_resource_map<std::wstring, GeometryShader>&>;
+
+
+	//----------------------------------------------------------------------------------
+	// Member Functions - Hull Shader
+	//----------------------------------------------------------------------------------
+
 	// Create a resource, or retrieve it if it already exists.
 	template<typename ResourceT>
 	[[nodiscard]]
 	auto acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
 	    -> std::enable_if_t<std::is_same_v<HullShader, ResourceT>, std::shared_ptr<HullShader>>;
+
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+	    -> std::enable_if_t<std::is_same_v<HullShader, ResourceT>, const shader_resource_map<std::wstring, HullShader>&>;
+
+
+	//----------------------------------------------------------------------------------
+	// Member Functions - Pixel Shader
+	//----------------------------------------------------------------------------------
 
 	// Create a resource, or retrieve it if it already exists.
 	template<typename ResourceT>
@@ -133,13 +187,28 @@ public:
 	auto acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
 	    -> std::enable_if_t<std::is_same_v<PixelShader, ResourceT>, std::shared_ptr<PixelShader>>;
 
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+	    -> std::enable_if_t<std::is_same_v<PixelShader, ResourceT>, const shader_resource_map<std::wstring, PixelShader>&>;
+
+
+	//----------------------------------------------------------------------------------
+	// Member Functions - Vertex Shader
+	//----------------------------------------------------------------------------------
+
 	// Create a resource, or retrieve it if it already exists.
 	template<typename ResourceT>
 	[[nodiscard]]
 	auto acquire(const std::wstring& guid,
-	                 const ShaderBytecode& bytecode,
-	                 gsl::span<const D3D11_INPUT_ELEMENT_DESC> input_element_descs)
+	             const ShaderBytecode& bytecode,
+	             gsl::span<const D3D11_INPUT_ELEMENT_DESC> input_element_descs)
 	    -> std::enable_if_t<std::is_same_v<VertexShader, ResourceT>, std::shared_ptr<VertexShader>>;
+
+	template<typename ResourceT>
+	[[nodiscard]]
+	auto getResourceMap() const
+	    -> std::enable_if_t<std::is_same_v<VertexShader, ResourceT>, const shader_resource_map<std::wstring, VertexShader>&>;
 
 
 private:
--- Rendering/src/resource/resource_mgr.tpp
+++ Rendering/src/resource/resource_mgr.tpp
@@ -12,11 +12,17 @@ auto ResourceMgr::acquire(const std::wstring& filename, const ModelConfig<Vertex
 
 template<typename ResourceT, typename VertexT>
 auto ResourceMgr::acquire(const std::wstring& name,
-	                          const ModelOutput& model_data,
-	                          const ModelConfig<VertexT>& config)
+	                      const ModelOutput& model_data,
+	                      const ModelConfig<VertexT>& config)
     -> std::enable_if_t<std::is_same_v<ModelBlueprint, ResourceT>, std::shared_ptr<ModelBlueprint>> {
 
 	return models.acquire(name, device, model_data, config);
+}
+
+template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+	-> std::enable_if_t<std::is_same_v<ModelBlueprint, ResourceT>, const resource_map<std::wstring, ModelBlueprint>&> {
+	return models;
 }
 
 
@@ -34,11 +40,17 @@ auto ResourceMgr::acquire(const std::wstring& filename)
 
 template<typename ResourceT>
 auto ResourceMgr::acquire(const std::wstring& name,
-	                          const D3D11_TEXTURE2D_DESC& desc, 
-	                          const D3D11_SUBRESOURCE_DATA& init_data)
+	                      const D3D11_TEXTURE2D_DESC& desc, 
+	                      const D3D11_SUBRESOURCE_DATA& init_data)
     -> std::enable_if_t<std::is_same_v<Texture, ResourceT>, std::shared_ptr<Texture>> {
 
 	return textures.acquire(name, name, device, desc, init_data);
+}
+
+template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+	-> std::enable_if_t<std::is_same_v<Texture, ResourceT>, const resource_map<std::wstring, Texture>&> {
+	return textures;
 }
 
 
@@ -54,10 +66,15 @@ auto ResourceMgr::acquire(const std::wstring& filename)
 	return fonts.acquire(filename, device, filename.c_str());
 }
 
+template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+	-> std::enable_if_t<std::is_same_v<Font, ResourceT>, const resource_map<std::wstring, Font>&> {
+	return fonts;
+}
 
 
 //----------------------------------------------------------------------------------
-// Shader
+// Compute Shader
 //----------------------------------------------------------------------------------
 
 template<typename ResourceT>
@@ -68,11 +85,33 @@ auto ResourceMgr::acquire(const std::wstring& guid, const ShaderBytecode& byteco
 }
 
 template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+    -> std::enable_if_t<std::is_same_v<ComputeShader, ResourceT>, const shader_resource_map<std::wstring, ComputeShader>&> {
+	return compute_shaders;
+}
+
+
+//----------------------------------------------------------------------------------
+// Domain Shader
+//----------------------------------------------------------------------------------
+
+template<typename ResourceT>
 auto ResourceMgr::acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
     -> std::enable_if_t<std::is_same_v<DomainShader, ResourceT>, std::shared_ptr<DomainShader>> {
 	
 	return domain_shaders.acquire(guid, guid, device, bytecode);
 }
+
+template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+    -> std::enable_if_t<std::is_same_v<DomainShader, ResourceT>, const shader_resource_map<std::wstring, DomainShader>&> {
+	return domain_shaders;
+}
+
+
+//----------------------------------------------------------------------------------
+// Geometry Shader
+//----------------------------------------------------------------------------------
 
 template<typename ResourceT>
 auto ResourceMgr::acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
@@ -82,11 +121,33 @@ auto ResourceMgr::acquire(const std::wstring& guid, const ShaderBytecode& byteco
 }
 
 template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+    -> std::enable_if_t<std::is_same_v<GeometryShader, ResourceT>, const shader_resource_map<std::wstring, GeometryShader>&> {
+	return geometry_shaders;
+}
+
+
+//----------------------------------------------------------------------------------
+// Hull Shader
+//----------------------------------------------------------------------------------
+
+template<typename ResourceT>
 auto ResourceMgr::acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
     -> std::enable_if_t<std::is_same_v<HullShader, ResourceT>, std::shared_ptr<HullShader>> {
 	
 	return hull_shaders.acquire(guid, guid, device, bytecode);
 }
+
+template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+    -> std::enable_if_t<std::is_same_v<HullShader, ResourceT>, const shader_resource_map<std::wstring, HullShader>&> {
+	return hull_shaders;
+}
+
+
+//----------------------------------------------------------------------------------
+// Pixel Shader
+//----------------------------------------------------------------------------------
 
 template<typename ResourceT>
 auto ResourceMgr::acquire(const std::wstring& guid, const ShaderBytecode& bytecode)
@@ -96,10 +157,27 @@ auto ResourceMgr::acquire(const std::wstring& guid, const ShaderBytecode& byteco
 }
 
 template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+    -> std::enable_if_t<std::is_same_v<PixelShader, ResourceT>, const shader_resource_map<std::wstring, PixelShader>&> {
+	return pixel_shaders;
+}
+
+
+//----------------------------------------------------------------------------------
+// Vertex Shader
+//----------------------------------------------------------------------------------
+
+template<typename ResourceT>
 auto ResourceMgr::acquire(const std::wstring& guid,
-	                          const ShaderBytecode& bytecode,
-	                          gsl::span<const D3D11_INPUT_ELEMENT_DESC> input_element_descs)
+	                      const ShaderBytecode& bytecode,
+	                      gsl::span<const D3D11_INPUT_ELEMENT_DESC> input_element_descs)
     -> std::enable_if_t<std::is_same_v<VertexShader, ResourceT>, std::shared_ptr<VertexShader>> {
 	
 	return vertex_shaders.acquire(guid, guid, device, bytecode, input_element_descs);
+}
+
+template<typename ResourceT>
+auto ResourceMgr::getResourceMap() const
+    -> std::enable_if_t<std::is_same_v<VertexShader, ResourceT>, const shader_resource_map<std::wstring, VertexShader>&> {
+	return vertex_shaders;
 }
