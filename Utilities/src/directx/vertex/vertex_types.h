07c1e3eeeb06688ee6bb5fdff27e2eb2fadda9f9
Connor <cbroyles94@gmail.com>
2019-03-17 22:19:58 -0400

    Moved the DirectX vertex input element definitions to the header, and made them constexpr.

--- Utilities/src/directx/vertex/vertex_types.h
+++ Utilities/src/directx/vertex/vertex_types.h
@@ -4,7 +4,13 @@
 #include "datatypes/vector_types.h"
 
 
+//----------------------------------------------------------------------------------
+// Position
+//----------------------------------------------------------------------------------
 struct VertexPosition final {
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	constexpr VertexPosition() noexcept = default;
 	constexpr VertexPosition(const VertexPosition& vertex) noexcept = default;
 	constexpr VertexPosition(VertexPosition&& vertex) noexcept = default;
@@ -13,27 +19,51 @@ struct VertexPosition final {
 		: position(position) {
 	}
 
+	
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~VertexPosition() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	constexpr VertexPosition& operator=(const VertexPosition& vertex) noexcept = default;
 	constexpr VertexPosition& operator=(VertexPosition&& vertex) noexcept = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	static constexpr bool hasNormal() noexcept { return false; }
 	static constexpr bool hasColor() noexcept { return false; }
 	static constexpr bool hasTexture() noexcept { return false; }
 
 
 	//----------------------------------------------------------------------------------
-	// Members
+	// Member Variables
 	//----------------------------------------------------------------------------------
 	f32_3 position;
 
-	static const u32 input_element_count = 1;
-	static const D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count];
+	static constexpr u32 input_element_count = 1;
+	static constexpr D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
 };
 
+static_assert(sizeof(VertexPosition) == 12, "Vertex struct/layout mismatch");
 
+
+
+
+//----------------------------------------------------------------------------------
+// Position/Color
+//----------------------------------------------------------------------------------
 struct VertexPositionColor final {
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionColor() noexcept = default;
 	constexpr VertexPositionColor(const VertexPositionColor& vertex) noexcept = default;
 	constexpr VertexPositionColor(VertexPositionColor&& vertex) noexcept = default;
@@ -43,28 +73,53 @@ struct VertexPositionColor final {
 		, color(color) {
 	}
 
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~VertexPositionColor() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionColor& operator=(const VertexPositionColor& vertex) noexcept = default;
 	constexpr VertexPositionColor& operator=(VertexPositionColor&& vertex) noexcept = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	static constexpr bool hasNormal() noexcept { return false; }
 	static constexpr bool hasColor() noexcept { return true; }
 	static constexpr bool hasTexture() noexcept { return false; }
 
 
 	//----------------------------------------------------------------------------------
-	// Members
+	// Member Variables
 	//----------------------------------------------------------------------------------
 	f32_3 position;
 	f32_4 color;
 
-	static const u32 input_element_count = 2;
-	static const D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count];
+	static constexpr u32 input_element_count = 2;
+	static constexpr D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
 };
 
+static_assert(sizeof(VertexPositionColor) == 28, "Vertex struct/layout mismatch");
 
+
+
+
+//----------------------------------------------------------------------------------
+// Position/Texture
+//----------------------------------------------------------------------------------
 struct VertexPositionTexture final {
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionTexture() noexcept = default;
 	constexpr VertexPositionTexture(const VertexPositionTexture& vertex) noexcept = default;
 	constexpr VertexPositionTexture(VertexPositionTexture&& vertex) noexcept = default;
@@ -74,28 +129,53 @@ struct VertexPositionTexture final {
 		, texCoord(texCoord) {
 	}
 
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~VertexPositionTexture() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionTexture& operator=(const VertexPositionTexture& vertex) noexcept = default;
 	constexpr VertexPositionTexture& operator=(VertexPositionTexture&& vertex) noexcept = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	static constexpr bool hasNormal() noexcept { return false; }
 	static constexpr bool hasColor() noexcept { return false; }
 	static constexpr bool hasTexture() noexcept { return true; }
 
 
 	//----------------------------------------------------------------------------------
-	// Members
+	// Member Variables
 	//----------------------------------------------------------------------------------
 	f32_3 position;
 	f32_2 texCoord;
 
-	static const u32 input_element_count = 2;
-	static const D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count];
+	static constexpr u32 input_element_count = 2;
+	static constexpr D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
 };
 
+static_assert(sizeof(VertexPositionColor) == 28, "Vertex struct/layout mismatch");
 
+
+
+
+//----------------------------------------------------------------------------------
+// Position/Texture/Texture
+//----------------------------------------------------------------------------------
 struct VertexPositionDualTexture final {
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionDualTexture() noexcept = default;
 	constexpr VertexPositionDualTexture(const VertexPositionDualTexture& vertex) noexcept = default;
 	constexpr VertexPositionDualTexture(VertexPositionDualTexture&& vertex) noexcept = default;
@@ -108,29 +188,55 @@ struct VertexPositionDualTexture final {
 		, texCoord1(texCoord1) {
 	}
 
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~VertexPositionDualTexture() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionDualTexture& operator=(const VertexPositionDualTexture& vertex) noexcept = default;
 	constexpr VertexPositionDualTexture& operator=(VertexPositionDualTexture&& vertex) noexcept = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	static constexpr bool hasNormal() noexcept { return false; }
 	static constexpr bool hasColor() noexcept { return false; }
 	static constexpr bool hasTexture() noexcept { return true; }
 
 
 	//----------------------------------------------------------------------------------
-	// Members
+	// Member Variables
 	//----------------------------------------------------------------------------------
 	f32_3 position;
 	f32_2 texCoord0;
 	f32_2 texCoord1;
 
-	static const u32 InputElementCount = 3;
-	static const D3D11_INPUT_ELEMENT_DESC input_elements[InputElementCount];
+	static constexpr u32 InputElementCount = 3;
+	static constexpr D3D11_INPUT_ELEMENT_DESC input_elements[InputElementCount] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"TEXCOORD", 1, DXGI_FORMAT_R32G32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
 };
 
+static_assert(sizeof(VertexPositionDualTexture) == 28, "Vertex struct/layout mismatch");
 
+
+
+
+//----------------------------------------------------------------------------------
+// Position/Normal
+//----------------------------------------------------------------------------------
 struct VertexPositionNormal final {
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionNormal() noexcept = default;
 	constexpr VertexPositionNormal(const VertexPositionNormal& vertex) noexcept = default;
 	constexpr VertexPositionNormal(VertexPositionNormal&& vertex) noexcept = default;
@@ -140,28 +246,53 @@ struct VertexPositionNormal final {
 		, normal(normal) {
 	}
 
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~VertexPositionNormal() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionNormal& operator=(const VertexPositionNormal& vertex) noexcept = default;
 	constexpr VertexPositionNormal& operator=(VertexPositionNormal&& vertex) noexcept = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	static constexpr bool hasNormal() noexcept { return true; }
 	static constexpr bool hasColor() noexcept { return false; }
 	static constexpr bool hasTexture() noexcept { return false; }
 
 
 	//----------------------------------------------------------------------------------
-	// Members
+	// Member Variables
 	//----------------------------------------------------------------------------------
 	f32_3 position;
 	f32_3 normal;
 
-	static const u32 input_element_count = 2;
-	static const D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count];
+	static constexpr u32 input_element_count = 2;
+	static constexpr D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
 };
 
+static_assert(sizeof(VertexPositionNormal) == 24, "Vertex struct/layout mismatch");
 
+
+
+
+//----------------------------------------------------------------------------------
+// Position/Normal/Color
+//----------------------------------------------------------------------------------
 struct VertexPositionNormalColor final {
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionNormalColor() noexcept = default;
 	constexpr VertexPositionNormalColor(const VertexPositionNormalColor& vertex) noexcept = default;
 	constexpr VertexPositionNormalColor(VertexPositionNormalColor&& vertex) noexcept = default;
@@ -174,29 +305,55 @@ struct VertexPositionNormalColor final {
 		, color(color) {
 	}
 
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~VertexPositionNormalColor() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionNormalColor& operator=(const VertexPositionNormalColor& vertex) noexcept = default;
 	constexpr VertexPositionNormalColor& operator=(VertexPositionNormalColor&& vertex) noexcept = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	static constexpr bool hasNormal() noexcept { return true; }
 	static constexpr bool hasColor() noexcept { return true; }
 	static constexpr bool hasTexture() noexcept { return false; }
 
 
 	//----------------------------------------------------------------------------------
-	// Members
+	// Member Variables
 	//----------------------------------------------------------------------------------
 	f32_3 position;
 	f32_3 normal;
 	f32_4 color;
 
-	static const u32 input_element_count = 3;
-	static const D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count];
+	static constexpr u32 input_element_count = 3;
+	static constexpr D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
 };
 
+static_assert(sizeof(VertexPositionNormalColor) == 40, "Vertex struct/layout mismatch");
 
+
+
+
+//----------------------------------------------------------------------------------
+// Position/Normal/Texture
+//----------------------------------------------------------------------------------
 struct VertexPositionNormalTexture final {
+	//----------------------------------------------------------------------------------
+	// Constructors
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionNormalTexture() noexcept = default;
 	constexpr VertexPositionNormalTexture(const VertexPositionNormalTexture& vertex) noexcept = default;
 	constexpr VertexPositionNormalTexture(VertexPositionNormalTexture&& vertex) noexcept = default;
@@ -209,23 +366,41 @@ struct VertexPositionNormalTexture final {
 		, texCoord(texCoord) {
 	}
 
+
+	//----------------------------------------------------------------------------------
+	// Destructor
+	//----------------------------------------------------------------------------------
 	~VertexPositionNormalTexture() = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Operators
+	//----------------------------------------------------------------------------------
 	constexpr VertexPositionNormalTexture& operator=(const VertexPositionNormalTexture& vertex) noexcept = default;
 	constexpr VertexPositionNormalTexture& operator=(VertexPositionNormalTexture&& vertex) noexcept = default;
 
+
+	//----------------------------------------------------------------------------------
+	// Member Functions
+	//----------------------------------------------------------------------------------
 	static constexpr bool hasNormal() noexcept { return true; }
 	static constexpr bool hasColor() noexcept { return false; }
 	static constexpr bool hasTexture() noexcept { return true; }
 
 
 	//----------------------------------------------------------------------------------
-	// Members
+	// Member Variables
 	//----------------------------------------------------------------------------------
 	f32_3 position;
 	f32_3 normal;
 	f32_2 texCoord;
 
-	static const u32 input_element_count = 3;
-	static const D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count];
+	static constexpr u32 input_element_count = 3;
+	static constexpr D3D11_INPUT_ELEMENT_DESC input_elements[input_element_count] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
 };
+
+static_assert(sizeof(VertexPositionNormalTexture) == 32, "Vertex struct/layout mismatch");
