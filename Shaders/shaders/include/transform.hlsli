98f1b94413259e3a287b93e817a5d303d4454faf
Connor <cbroyles94@gmail.com>
2019-05-04 15:20:48 -0400

    Refactor

--- Shaders/shaders/include/transform.hlsli
+++ Shaders/shaders/include/transform.hlsli
@@ -16,9 +16,9 @@ float4 Transform(float3 position,
 				 matrix world_to_view,
 				 matrix view_to_projection) {
 
-	float3 world = mul(float4(position, 1.0f), object_to_world).xyz;
-	float3 view  = mul(float4(world, 1.0f), world_to_view).xyz;
-	float4 proj  = mul(float4(view, 1.0f), view_to_projection);
+	const float3 world = mul(float4(position, 1.0f), object_to_world).xyz;
+	const float3 view  = mul(float4(world, 1.0f), world_to_view).xyz;
+	const float4 proj  = mul(float4(view, 1.0f), view_to_projection);
 
 	return proj;
 }
--- Shaders/shaders/skybox/skybox_vs.hlsl
+++ Shaders/shaders/skybox/skybox_vs.hlsl
@@ -3,11 +3,14 @@
 
 
 PSWorldPosition VS(uint vertex : SV_VertexID) {
-	PSWorldPosition vout;
 
-	vout.p_world = geosphere[vertex];
-	const float3 p      = mul(vout.p_world, (float3x3)g_world_to_camera);
-	vout.p       = mul(float4(p, 1.0f), g_camera_to_projection).xyww;
+	const float3 p_world = geosphere[vertex];
+	const float3 p_view  = mul(p_world, (float3x3)g_world_to_camera);
+	const float4 p_proj  = mul(float4(p_view, 1.0f), g_camera_to_projection).xyww;
+
+	PSWorldPosition vout;
+	vout.p_world = p_world;
+	vout.p       = p_proj;
 
 	return vout;
 }
