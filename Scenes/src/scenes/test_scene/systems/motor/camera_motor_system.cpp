1319f48bab773e294a771dba3a86dbf37b06bd34
Connor <cbroyles94@gmail.com>
2019-06-08 19:00:23 -0400

    Refactor

--- Scenes/src/scenes/test_scene/systems/motor/camera_motor_system.cpp
+++ Scenes/src/scenes/test_scene/systems/motor/camera_motor_system.cpp
@@ -153,14 +153,12 @@ void CameraMotorSystem::move(CameraMovement& mv, Transform& transform) const {
 
 	// Limit veloctiy to maximum
 	if (velocity_mag > mv.getMaxVelocity()) {
-
 		velocity_vec = XMVector3Normalize(velocity_vec) * mv.getMaxVelocity();
 		mv.setVelocity(velocity_vec);
 	}
 
 
 	if (velocity_mag != 0.0f) {
-
 		const auto dt = static_cast<f32>(dtSinceLastUpdate().count());
 
 		// Move the camera
@@ -179,16 +177,13 @@ void CameraMotorSystem::move(CameraMovement& mv, Transform& transform) const {
 
 void CameraMotorSystem::decelerate(CameraMovement& mv) const {
 
-	f32   decel_amount;
 	f32_3 velocity = mv.getVelocity();
-
 	const auto dt = static_cast<f32>(dtSinceLastUpdate().count());
 
 	// Decelerate in each direction if not moving in that
 	// direction and the current velocity isn't 0.
 	if (!mv.isMovingX() && velocity[0] != 0.0f) {
-
-		decel_amount = copysign(1.0f, velocity[0]) * mv.getDeceleration() * dt;
+		const f32 decel_amount = copysign(1.0f, velocity[0]) * mv.getDeceleration() * dt;
 
 		if (abs(decel_amount) > abs(velocity[0])) {
 			velocity[0] = 0.0f;
@@ -199,8 +194,7 @@ void CameraMotorSystem::decelerate(CameraMovement& mv) const {
 	}
 
 	if (!mv.isMovingY() && velocity[1] != 0.0f) {
-
-		decel_amount = copysign(1.0f, velocity[1]) * mv.getDeceleration() * dt;
+		const f32 decel_amount = copysign(1.0f, velocity[1]) * mv.getDeceleration() * dt;
 
 		if (abs(decel_amount) > abs(velocity[1])) {
 			velocity[1] = 0.0f;
@@ -211,8 +205,7 @@ void CameraMotorSystem::decelerate(CameraMovement& mv) const {
 	}
 
 	if (!mv.isMovingZ() && velocity[2] != 0.0f) {
-
-		decel_amount = copysign(1.0f, velocity[2]) * mv.getDeceleration() * dt;
+		const f32 decel_amount = copysign(1.0f, velocity[2]) * mv.getDeceleration() * dt;
 
 		if (abs(decel_amount) > abs(velocity[2])) {
 			velocity[2] = 0.0f;
