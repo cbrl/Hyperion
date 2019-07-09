4bfe23ba253d65caa85ce687ad22c9431961682e
Connor <cbroyles94@gmail.com>
2019-07-09 17:47:21 -0400

    Renamed config tokens

--- Engine/src/config/config_tokens.h
+++ Engine/src/config/config_tokens.h
@@ -5,23 +5,23 @@
 
 namespace ConfigTokens {
 	// Window title
-	constexpr gsl::czstring<> win_title = "window title";
+	constexpr gsl::czstring<> win_title = "WindowTitle";
 
 	// Display config tokens
 	constexpr gsl::czstring<> display_config = "display";
-	constexpr gsl::czstring<> display_width  = "display width";
-	constexpr gsl::czstring<> display_height = "display height";
-	constexpr gsl::czstring<> refresh        = "refresh rate";
-	constexpr gsl::czstring<> vsync          = "vsync";
-	constexpr gsl::czstring<> fullscreen     = "fullscreen";
-	constexpr gsl::czstring<> aa_type        = "anti aliasing";
+	constexpr gsl::czstring<> display_width  = "Width";
+	constexpr gsl::czstring<> display_height = "Height";
+	constexpr gsl::czstring<> refresh        = "RefreshRate";
+	constexpr gsl::czstring<> vsync          = "VSync";
+	constexpr gsl::czstring<> fullscreen     = "Fullscreen";
+	constexpr gsl::czstring<> aa_type        = "AntiAliasing";
 
 	// Rendering config tokens
 	constexpr gsl::czstring<> render_config                = "rendering";
-	constexpr gsl::czstring<> smap_res                     = "shadow map resolution";
-    constexpr gsl::czstring<> smap_depth_bias              = "shadow map depth bias";
-    constexpr gsl::czstring<> smap_slope_scaled_depth_bias = "shadow map slope scaled depth bias";
-    constexpr gsl::czstring<> smap_depth_bias_clamp        = "shadow map depth bias clamp";
+	constexpr gsl::czstring<> smap_res                     = "ShadowMapResolution";
+    constexpr gsl::czstring<> smap_depth_bias              = "ShadowMapDepthBias";
+    constexpr gsl::czstring<> smap_slope_scaled_depth_bias = "ShadowMapSlopeScaledDepthBias";
+    constexpr gsl::czstring<> smap_depth_bias_clamp        = "ShadowMapDepthBiasClamp";
 
 	// Input config tokens
     constexpr gsl::czstring<> key_config = "input";
