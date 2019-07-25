eabf5d99b3ab66ba80e0ae5a53baa848494b1e41
Connor <cbroyles94@gmail.com>
2019-07-25 16:34:50 -0400

    Modified shadow alpha minimum threshold

--- Shaders/src/hlsl.h
+++ Shaders/src/hlsl.h
@@ -11,8 +11,8 @@
 #define ALPHA_MIN 0.00392156f
 
 // The minimum alpha value at which a shadow will still be cast
-// 16/255
-#define ALPHA_SHADOW_THRESHOLD 0.06274509f
+// 8/255
+#define ALPHA_SHADOW_THRESHOLD 0.03137254f
 
 
 //----------------------------------------------------------------------------------
